d3608cfc9ebb30c093bfcada9089eee4ca33d740
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-18 01:54:25 +0530

    [Avinash] : Added proper comments to make the program easier to understand

--- gcr-codebase/programming-elements/Level-1/CalculateProfitLoss.java
+++ gcr-codebase/programming-elements/Level-1/CalculateProfitLoss.java
@@ -1,10 +1,14 @@
+// Writing a java program to calculate profit and profit percent
 public class CalculateProfitLoss {
 	public static void main(String args[]) {
+		// Giving sell price and cost price
 		int costPrice = 129, sellingPrice = 191;
 		
+		// calculating profit and percent profit
 		int profit = sellingPrice - costPrice;
 		double profitPercent = (double)profit / (double)costPrice * 100.0;
 		
+		// Displaying cost price, sell price, profit, and percent profit
 		System.out.println("The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice);
 		System.out.println("The Profit is INR " + profit + " and the Profit Percentage is " + profitPercent);
 	}
