6727a49e5b44b9b5e9361a658658e694bcf69bd2
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-17 15:14:57 +0530

    [Avinash] : Added a program to calculate average marks of Sam in math, physics, ans chemistry

---
+++ gcr-codebase/programming-elements/Level-1/CalculateAverageMarks.java
@@ -0,0 +1,8 @@
+public class CalculateAverageMarks {
+	public static void main(String args[]) {		
+		int mathMarks = 94, physicsMarks = 95, chemistryMarks = 97;
+		
+		double averageMarks = (mathMarks + physicsMarks + chemistryMarks) / 3.0;
+		System.out.println("Average of marks in math, physics, and chemistry is : " + averageMarks);
+	}
+}
