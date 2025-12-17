60b278fe6978a2589aa05f3b00fc570507b6a782
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-18 01:49:40 +0530

    [Avinash] : Added proper comments in the code

--- gcr-codebase/programming-elements/Level-1/CalculateAverageMarks.java
+++ gcr-codebase/programming-elements/Level-1/CalculateAverageMarks.java
@@ -1,8 +1,13 @@
+// Writing a java program to calculate average marks in math, physics, and chemistry 
 public class CalculateAverageMarks {
 	public static void main(String args[]) {		
+		// Providing marks of each subject 
 		int mathMarks = 94, physicsMarks = 95, chemistryMarks = 97; 
 		
+		// Calculating average marks 
 		double averageMarks = (mathMarks + physicsMarks + chemistryMarks) / 3.0;
+		
+		// Displaying average marks in math, physics, and chemistry
 		System.out.println("Average marks in math, physics, and chemistry is : " + averageMarks);
 	}
 }
