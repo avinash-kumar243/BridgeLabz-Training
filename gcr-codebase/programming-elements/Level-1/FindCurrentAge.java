35599d552a955464d3c9ac6ece65ad4b64b5e714
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-18 01:57:52 +0530

    [Avinash] : Added proper comments to make it easier to understand

--- gcr-codebase/programming-elements/Level-1/FindCurrentAge.java
+++ gcr-codebase/programming-elements/Level-1/FindCurrentAge.java
@@ -1,12 +1,15 @@
+// Writing a java program to find current age of a person
 import java.util.Scanner;
 public class FindCurrentAge {
 	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		
+		// Providing birth year and current year
 		int birthYear = 2000;
 		int currentYear = 2024;
-		
+
+		// Calculating current age
 		int currentAge = currentYear - birthYear;
+		
+		// Displaying current age of Harry
 		System.out.println("Age of Harry is : " + currentAge);
 	}
 }
