f61e93dca1a853eda3e9e93d368ede27cadcdbd9
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-17 12:43:22 +0530

    [Avinash] : Added a program to calculate perimeter of a rectangle

---
+++ gcr-codebase/Program-prerequisites/CalculatePerimeterOfRectangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CalculatePerimeterOfRectangle {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter Length: ");
+		int length = sc.nextInt();
+		
+		System.out.print("Enter Width: ");
+		int width = sc.nextInt();
+		
+		int perimeter = 2 * (length + width);
+		System.out.println("Perimeter of Rectangle is : " + perimeter);
+	}
+}
