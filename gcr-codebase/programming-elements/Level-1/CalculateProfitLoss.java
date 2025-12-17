26361adb7d4ca5e158bb72c511e480a53f777271
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-17 16:38:02 +0530

    [Avinash] : Added type casting in the program for calculating profit and loss

--- gcr-codebase/programming-elements/Level-1/CalculateProfitLoss.java
+++ gcr-codebase/programming-elements/Level-1/CalculateProfitLoss.java
@@ -3,7 +3,7 @@ public class CalculateProfitLoss {
 		int costPrice = 129, sellingPrice = 191;
 		
 		int profit = sellingPrice - costPrice;
-		double profitPercent = (double)profit / (costPrice * 100.0);
+		double profitPercent = (double)profit / (double)costPrice * 100.0;
 		
 		System.out.println("The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice);
 		System.out.println("The Profit is INR " + profit + " and the Profit Percentage is " + profitPercent);
