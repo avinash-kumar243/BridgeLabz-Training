da5a8a58783ca40b3f8953fe1fec4a7514bd1c2f
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-18 14:20:06 +0530

    [Avinash] : Added a program to find if a person is eligible for vote or not

---
+++ gcr-codebase/control-flow/Level-1/IsEligibleForVote.java
@@ -0,0 +1,12 @@
+// Writing a program to check if a person can vote or not
+import java.util.Scanner;
+public class IsEligibleForVote {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter age : ");
+		int age = sc.nextInt();
+		
+		if(age >= 18) System.out.println("The person's age is " + age + " and can vote.");
+		else System.out.println("The person's age is " + age + " and cannot vote.");
+	}
+}
