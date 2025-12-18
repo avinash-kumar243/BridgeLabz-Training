b08b398e85d7e5e4f0b9168c524be6b115bb737c
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-18 16:36:51 +0530

    [Avinash] : Added a program to calculate bonus on salary when year of service is more than 5

---
+++ gcr-codebase/control-flow/Level-1/SalaryBonus.java
@@ -0,0 +1,17 @@
+// Writing a program to find the bonus on salary of employees, based on their years of service
+import java.util.Scanner;
+public class SalaryBonus {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter salary : ");
+		int salary = sc.nextInt();
+		System.out.println("Enter year of service : ");
+		int serviceYear = sc.nextInt();
+		
+		double bonus = 0;
+		if(serviceYear > 5) bonus = (double)salary * 0.05;
+		
+		System.out.println(bonus);
+	}
+}
