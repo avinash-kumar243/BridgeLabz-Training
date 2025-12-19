c7505a2d25144caf1fd900239e01b35d8fe50618
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-19 16:21:41 +0530

    [Avinash] : Added a program to display numbers and calculate sum

---
+++ gcr-codebase/arrays/Level-1/DisplayNumbers.java
@@ -0,0 +1,24 @@
+// Writing a program to display numbers and calculate sum of all numbers 
+import java.util.Scanner;
+public class DisplayNumbers {
+	public static void main(String args[]) { 
+		Scanner sc = new Scanner(System.in);
+		
+		double arr[] = new double[10];
+		double total = 0.0;
+		int idx = 0;
+		
+		System.out.println("Enter numbers : ");
+		while(true) {
+			double num = sc.nextDouble();
+			if(num <= 0) break;
+			arr[idx] = num;
+			idx++;
+			if(idx == 10) break;
+		}
+		for(int i=0; i<arr.length; i++) {
+			total += arr[i];
+		}
+		System.out.println("Sum of all numbers are : " + total);
+	}
+}
