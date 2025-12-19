45f28581c5490c5a45d2d803478edf8577e48154
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-19 08:42:12 +0530

    [Avinash] : Added a program to determine leap year or not with a single if condition

---
+++ gcr-codebase/control-flow/Level-3/LeapYearWithSingleIf.java
@@ -0,0 +1,15 @@
+// Writing a program to determine leap year or not with a single if condition
+import java.util.Scanner;
+public class LeapYearWithSingleIf {
+	public static void main(String args[]) { 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Year");
+		int year = sc.nextInt();
+		
+		if(year >= 1582) System.out.println("Valid year to check for leap year");
+		
+		if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) System.out.println(year + " is a leap year");
+		else System.out.println(year + " is not a leap year");
+	}
+}
