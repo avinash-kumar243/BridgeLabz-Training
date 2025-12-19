8ebd4b5d4d5fd2af1654bd128a6d875f85fe35e8
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-19 16:33:07 +0530

    [Avinash] : Added a program to print multiplication table using arrays

---
+++ gcr-codebase/arrays/Level-1/MultiplicationTableUsingArray.java
@@ -0,0 +1,18 @@
+// Writing a program to print multiplication table using arrays
+import java.util.Scanner;
+public class MultiplicationTableUsingArray {
+	public static void main(String args[]) { 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a number : "); 
+		int number = sc.nextInt();
+		int multiplicationResult[] = new int[4];
+		
+		for(int i=0; i<multiplicationResult.length; i++) {
+			multiplicationResult[i] = number * (i+6);
+		}
+		for(int i=0; i<multiplicationResult.length; i++) {
+			System.out.println(number + "*" + (i+6) + " = " + multiplicationResult[i]);
+		}		
+	}
+}
