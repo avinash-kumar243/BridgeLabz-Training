951764a5f895eba2c7fa4896b0c401e0f39b4398
Avinash kumar <avinashkumar40977@gmail.com>
2025-12-21 20:02:22 +0530

    [Avinash] : Updated the class name

--- gcr-codebase/methods/Level-2/UnitConvertor2.java
+++ gcr-codebase/methods/Level-2/UnitConvertor2.java
@@ -1,5 +1,5 @@
 // Writing a program to implement UnitConvertor program
-class UnitConvertor2 {
+class Conversion {
 	// Writing a method to convert yards to feet
 	public static double convertYardsToFeet(double yards) {
 		double yards2feet = 3;
