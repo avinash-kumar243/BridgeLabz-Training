5b86340fa39f2bdbc4a823250e0af58c3a322a1e
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-07 16:50:43 +0530

    [Avinash] : Added a program to find peak element in an array

--- dsa-practice/gcr-codebase/dsa/src/com/linearandbinarysearch/binarysearch/FindPeakElement.java
+++ dsa-practice/gcr-codebase/dsa/src/com/linearandbinarysearch/binarysearch/FindPeakElement.java
@@ -14,7 +14,7 @@ public class FindPeakElement {
             int mid = (st + end) / 2;
 
             if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
-            else if(nums[mid] < nums[mid-1]) end 	= mid - 1;
+            else if(nums[mid] < nums[mid-1]) end = mid - 1;
             else st = mid + 1;
         }
         return st;
