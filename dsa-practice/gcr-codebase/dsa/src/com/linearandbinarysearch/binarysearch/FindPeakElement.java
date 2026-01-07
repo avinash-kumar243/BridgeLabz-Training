06cdcd951048bd29ce993f641b46962c86204017
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-07 16:45:15 +0530

    [Avinash] : Added a program to find peak element in an array

---
+++ dsa-practice/gcr-codebase/dsa/src/com/linearandbinarysearch/binarysearch/FindPeakElement.java
@@ -0,0 +1,41 @@
+package com.linearandbinarysearch.binarysearch;
+
+import java.util.Scanner;
+
+public class FindPeakElement {
+	public static int findPeakElement(int nums[]) {
+		int n = nums.length;
+		
+		if(n >= 2 && nums[0] > nums[1]) return 0;
+		if(n >= 2 && nums[n-1] > nums[n-2]) return n-1;
+		
+		int st = 0, end = n-1;
+        while(st < end) { 
+            int mid = (st + end) / 2;
+
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            else if(nums[mid] < nums[mid-1]) end 	= mid - 1;
+            else st = mid + 1;
+        }
+        return st;
+	}
+	public static void main(String[] args) {			
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size of array: ");
+		int size = sc.nextInt();
+		sc.nextLine();
+
+		int arr[] = new int[size];
+		
+		int idx = 0;
+		System.out.println("Enter array elements of size " + size + " : ");
+		while(idx < arr.length) {
+			arr[idx] = sc.nextInt();
+			idx++;
+		}
+		
+		int peakElementIdx = findPeakElement(arr);
+		System.out.println("Peak Element is: " + arr[peakElementIdx]);
+		System.out.println("Peak Element index is: " + peakElementIdx);
+	}
+}
