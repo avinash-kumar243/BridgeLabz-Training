3ae8ac398b0faef71f306e0663801388f2e7e4ce
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-08 00:35:28 +0530

    [Avinash] : Added a program to find minimum element in sorted but roteted array

---
+++ leet-code-codebase/arrays/FindMinimunInSortedRotetedArray.java
@@ -0,0 +1,27 @@
+// 153. Find Minimum in Rotated Sorted Array
+
+// Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+// [4,5,6,7,0,1,2] if it was rotated 4 times.
+// [0,1,2,4,5,6,7] if it was rotated 7 times.
+// Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+// Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+
+
+class FindMinimunInSortedRotetedArray {
+    public int findMin(int[] nums) {
+        int st = 0, end = nums.length-1;
+        while(st <= end) {
+            int mid = (st + end) / 2;
+
+            if((nums[mid] > nums[st] || nums[mid] == nums[st]) && nums[st] > nums[end]) st = mid + 1;
+            else if(nums[mid] < nums[st] && nums[mid] < nums[end]) end = mid;
+            else {
+                end = mid - 1;
+            }
+        }
+        return nums[st];
+    }
+}
