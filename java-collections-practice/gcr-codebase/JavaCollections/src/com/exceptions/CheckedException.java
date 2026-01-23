44cb50280b1a7fcc2c467f431b893126f3e4d321
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-24 01:21:33 +0530

    [Avinash] : Added a program to handle IOException

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/exceptions/CheckedException.java
@@ -0,0 +1,19 @@
+package com.exceptions;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CheckedException {
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+			String line;
+			
+			while((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch(IOException e) {
+			System.out.println("IOException occurs: \nFile not found - " + e.getMessage());
+		}
+	}
+}
