278e26a1705a8995dae79076ddaeb57713d8f9a0
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-24 01:52:59 +0530

    [AVINASH] : Added a program to implement Custome Exception

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/exceptions/CustomException.java
@@ -0,0 +1,35 @@
+package com.exceptions;
+
+import java.util.Scanner;
+
+
+// Custom Exception class
+class InvalidAgeException extends ArithmeticException {
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
+public class CustomException {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your age: ");
+		int age = sc.nextInt();
+		
+		try {
+			validateAge(age);
+		} catch(InvalidAgeException e) {
+			System.out.println("Invalid age excption occurs: " + e.getMessage());
+		}
+	}
+
+	private static void validateAge(int age) {
+		if(age < 18 && age > 0) {
+			throw new InvalidAgeException("Age below 18 can't vote");
+		} else if(age <= 0) {
+			throw new InvalidAgeException("Age must be positive");
+		} else {
+			System.out.println("Access grantes! You can vote");
+		}
+	}
+}
