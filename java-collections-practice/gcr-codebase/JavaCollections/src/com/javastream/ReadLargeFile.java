ef5a775774406c0aec7e46880cde2867b8265301
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-26 19:46:22 +0530

    [Avinash] : Added a program to read a large file line by line

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javastream/ReadLargeFile.java
@@ -0,0 +1,23 @@
+package com.javastream;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadLargeFile {
+	public static void main(String[] args) {
+        String filePath = "largefile.log";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            
+        	String line;
+            while ((line = br.readLine()) != null) {
+                if(line.toLowerCase().contains("error")) {
+                    System.out.println(line);
+                }
+            }
+        } catch(IOException e) {
+            System.out.println("I/O Error: " + e.getMessage());
+        }
+	}
+}
