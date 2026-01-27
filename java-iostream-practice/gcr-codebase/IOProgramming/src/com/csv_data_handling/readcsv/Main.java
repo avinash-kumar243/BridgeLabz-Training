7606ec431b7bd2dc675bc28e65716f21a335f492
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-27 10:19:40 +0530

    [Avinash] : Added a program to read csv file of student

--- java-iostream-practice/gcr-codebase/IOProgramming/src/com/csv_data_handling/readcsv/Main.java
+++ java-iostream-practice/gcr-codebase/IOProgramming/src/com/csv_data_handling/readcsv/Main.java
@@ -1,7 +1,28 @@
 package com.csv_data_handling.readcsv;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 	public static void main(String[] args) {
+		String employeePath = "DataFiles/employee.csv";
+			
 		
+		try(BufferedReader br = new BufferedReader(new FileReader(employeePath))) {
+			
+			String line;
+			
+			// Read and skip header
+			br.readLine();
+			
+			while((line = br.readLine()) != null) {
+				String columns[] = line.split(",");
+				System.out.println("ID: " + columns[0] + ", Name: " + columns[1] + ", Age: " + columns[2] + ", Marks: " + columns[3]);
+			}
+		} catch(IOException e) {
+			System.out.println("IOException occurs: " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
---
+++ java-iostream-practice/gcr-codebase/IOProgramming/src/com/csv_data_handling/readcsv/Student.java
@@ -0,0 +1,17 @@
+package com.csv_data_handling.readcsv;
+
+public class Student {
+	int ID;
+	String name;
+	int age;
+	int marks;
+	
+	public Student(int iD, String name, int age, int marks) {
+		ID = iD;
+		this.name = name;
+		this.age = age;
+		this.marks = marks;
+	}
+	
+	
+}
