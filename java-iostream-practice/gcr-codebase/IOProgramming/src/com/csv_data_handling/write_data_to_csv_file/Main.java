2e05486513e6d578222d82dc9e8317b788531acd
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-27 12:24:11 +0530

    [Avinash] : Added a program to create a csv file of employee and write details

--- java-iostream-practice/gcr-codebase/IOProgramming/src/com/csv_data_handling/write_data_to_csv_file/Main.java
+++ java-iostream-practice/gcr-codebase/IOProgramming/src/com/csv_data_handling/write_data_to_csv_file/Main.java
@@ -1,7 +1,27 @@
 package com.csv_data_handling.write_data_to_csv_file;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 	public static void main(String[] args) {
+		String csvFile = "DataFiles/employee2.csv";
 		
+		try {
+			FileWriter file = new FileWriter(csvFile);
+			
+			file.write("ID, Name, Department, Salary\n"); 
+			file.write("101, Avinash, IT, 10000\n"); 
+			file.write("102, Akshay, CSE, 30000\n");
+			file.write("103, Deepanshu, AIML, 50000\n");
+			file.write("104, Priyanshu, IT, 25000\n");
+			file.write("105, Mohan, CD, 20000\n");
+			file.write("106, Kavya, AI, 32000\n");
+			
+			System.out.println("Students data written successfully");
+			file.close();
+		} catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
