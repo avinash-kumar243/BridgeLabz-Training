a3dc9d1c2565152861b9410a9cb34324fff3f383
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-28 16:45:37 +0530

    [Avinash] : Added an Employee class to perform java stream feature

--- java8-programming-practice/scenario-based-codebase/ScenarioPractice/src/com/questions/employee/EmployeeMain.java
+++ java8-programming-practice/scenario-based-codebase/ScenarioPractice/src/com/questions/employee/EmployeeMain.java
@@ -1,17 +1,19 @@
 package com.questions.employee;
 
+import java.util.Comparator;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class EmployeeMain {
 	public static void main(String[] args) {
 		EmployeeDataList employeeList = new EmployeeDataList();
 		
-		employeeList.add(new Employee(101, "Avi", 24, "Male", "EC", 2024, 30000));
-		employeeList.add(new Employee(102, "Mannu", 28, "Male", "CSE", 2024, 40000));
+		employeeList.add(new Employee(101, "Avi", 24, "Male", "EC", 2012, 30000));
+		employeeList.add(new Employee(102, "Mannu", 28, "Male", "CSE", 2018, 40000));
 		employeeList.add(new Employee(103, "Deepika", 25, "Female", "IT", 2025, 35000));
-		employeeList.add(new Employee(104, "Komal", 23, "Female", "CSE", 2024, 20000));
+		employeeList.add(new Employee(104, "Komal", 23, "Female", "CSE", 2014, 20000));
 		employeeList.add(new Employee(105, "Shekhar", 27, "Male", "EC", 2023, 50000));
-		employeeList.add(new Employee(106, "Vikki", 26, "Male", "CE", 2025, 60000));
+		employeeList.add(new Employee(106, "Vikki", 26, "Male", "CE", 2015, 60000));
 		
 		
 		// Actual work :- 
@@ -30,5 +32,12 @@ public class EmployeeMain {
 		// Problem 3 :- What is the average age of male and female employees ?
 		System.out.print("Average age of male and female are: "); 
 		System.out.println(employeeList.employees.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingInt(Employee::getAge))));
+		
+		
+		// Problem 4 :- Get the details of highest paid employee in the organization?
+		Optional<Employee> h = employeeList.employees.stream().collect(Collectors.maxBy(Comparator.comparing(Employee::getSalary)));
+		System.out.println(h);
+		
+		
 	}
 }
