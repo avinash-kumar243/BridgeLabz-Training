a0bbaded6383ee5f91c0dd3281249cf08eb674fc
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-29 10:39:01 +0530

    [Avinash] : Added a program to create a Student class and performoperations using java stream API

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioPractice/src/com/questions/student/Student.java
@@ -0,0 +1,60 @@
+package com.questions.student;
+
+public class Student {
+	private String name;
+	private int id;
+	private String firstName;
+	private String department;
+	private int age;
+	private String gender;
+	private String city;
+	private int rank;
+	private String contacts;
+	
+	public Student(String name, int id, String firstName, String department, int age, String gender, String city, int rank,String contacts) {
+		this.name = name;
+		this.id = id;
+		this.firstName = firstName;
+		this.department = department;
+		this.age = age;
+		this.gender = gender;
+		this.city = city;
+		this.rank = rank;
+		this.contacts = contacts;
+	}
+
+	// Getters for Student details
+	public String getName() {
+		return name;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public String getCity() {
+		return city;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public String getContacts() {
+		return contacts;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", id=" + id + ", firstName=" + firstName + ", department=" + department + ", age=" + age + ", gender="
+				+ gender + ", city=" + city + ", rank=" + rank + ", contacts=" + contacts + "]";
+	}
+}
---
+++ java8-programming-practice/scenario-based-codebase/ScenarioPractice/src/com/questions/student/StudentMain.java
@@ -0,0 +1,98 @@
+package com.questions.student;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class StudentMain {
+	public static void main(String[] args) {
+		List<Student> students = new ArrayList<>();
+		
+		students.add(new Student("Avinash kumar", 101, "Avinash", "CE", 23, "Male", "Bhopal", 100, "111111")); 
+		students.add(new Student("Krishna kumar", 102, "Krishna", "CSE", 25, "Male", "Noida", 140, "222222")); 
+		students.add(new Student("Deepika Roy", 103, "Deepika", "AIDS", 22, "Female", "Indore", 42, "333333")); 
+		students.add(new Student("Priyanshu kumar", 104, "Priyanshu", "IT", 23, "Male", "Mumbai", 410, "444444")); 
+		students.add(new Student("Deepak kumar", 105, "Deepak", "ME", 21, "Male", "Delhi", 25, "555555")); 
+		students.add(new Student("Komal kumari", 106, "Komal", "ME", 26, "Female", "Mumbai", 12, "666666")); 
+		students.add(new Student("Anjali Singh", 107, "Anjali", "CSE", 23, "Female", "Mumbai", 90, "777777")); 
+		students.add(new Student("Devika kumari", 108, "Devika", "EC", 24, "Female", "Bhopal", 78, "888888")); 
+		students.add(new Student("Rohan kumar", 109, "Rohan", "AIDS", 27, "Male", "Pune", 120, "999999")); 
+		
+
+		
+		// Problem 1 :- Print all students whose rank is less than 50
+		System.out.println("\n1. All students whose rank is less than 50 are: ");
+		students.stream()
+				.filter(Student -> Student.getRank() <= 50)
+				.forEach(System.out::println);
+		
+		
+		// Problem 2 :- Print all students whose age is greater than 25.
+		System.out.println("\n2. All students whose age is greater than 25 are: ");
+		students.stream()
+				.filter(student -> student.getAge() > 25)
+				.forEach(System.out::println);
+		
+		
+		// Problem 3 :- Print the names of all students only.
+		System.out.println("\n3. All students names are: ");
+		students.stream()
+				.map(Student :: getName)
+				.forEach(System.out::println);
+		
+		
+		// Problem 4 :- Print all students belonging to Mechanical Engineering.
+		System.out.println("\n4. All students belonging to Mechanical Engineering are: ");
+		students.stream()
+				.filter(Student -> Student.getDepartment().equals("ME"))
+				.forEach(System.out::println);
+		
+		
+		// Problem 5 :- Print all students not from Mumbai.
+		System.out.println("\n5. All students not from Mumbai are: ");
+		students.stream()
+				.filter(Student -> !Student.getCity().equals("Mumbai"))
+				.forEach(System.out::println);
+		
+		
+		
+		// Problem 6 :- Sort students by rank (ascending)
+		System.out.println("\n6. Sort students by rank (ascending).");
+		students.stream()
+				.sorted(Comparator.comparing(Student::getRank))
+				.forEach(System.out::println);
+		
+		
+		// Problem 7 :- Sort students by age (descending)
+		System.out.println("\n7. Sort students by age (descending).");
+		students.stream()
+				.sorted(Comparator.comparing(Student::getAge).reversed())
+				.forEach(System.out::println);
+		
+		
+		// Problem 8 :- Print top 5 students based on rank.
+		System.out.println("\n8. Print top 5 students based on rank.");
+		students.stream()
+				.sorted(Comparator.comparing(Student::getRank))
+				.limit(5)
+				.forEach(System.out::println);
+		
+		
+		// Problem 9 :- Print last 3 students based on rank.
+		System.out.println("\n9. Print last 3 students based on rank.");
+		students.stream()
+				.sorted(Comparator.comparing(Student::getRank).reversed())
+				.limit(3)
+				.forEach(System.out::println);
+		
+		
+		// Problem 10 :- Print students sorted by name and then rank.
+		System.out.println("\n10. Print students sorted by name and then rank.");
+		students.stream()
+				.sorted(Comparator.comparing(Student::getName).thenComparing(Student::getRank))
+				.forEach(System.out::println);
+		
+		
+		
+	}
+}
