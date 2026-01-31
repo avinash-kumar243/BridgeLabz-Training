151fdf70084430cd9d01f67b5f86ab0033a3bc89
Avinash kumar <avinashkumar40977@gmail.com>
2026-01-31 16:29:40 +0530

    [Avinash] : Added a program to validate username using regex

--- java-collections-practice/gcr-codebase/JavaCollections/src/com/regex/validateusername/Main.java
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/regex/validateusername/Main.java
@@ -1,7 +1,19 @@
 package com.regex.validateusername;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter input: "); 
+		String input = sc.next();
+		
+		String regex = "^[A-Za-z]\\w{4,14}$"; 
+		
+		boolean isValid = input.matches(regex);
+		
+		if(isValid) System.out.println("Valid");
+		else System.out.println("Invalid Input!");
 	}
 }
