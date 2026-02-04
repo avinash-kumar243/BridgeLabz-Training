a78f991e80d15b4a5d0f0cd32c80dfa665a516b3
Avinash kumar <avinashkumar40977@gmail.com>
2026-02-05 02:10:49 +0530

    [AVINASH] : Added a practice program of multithreading

---
+++ java8-programming-practice/gcr-codebase/Java8Features/src/com/multithreading/practice/Example5using_java8/Main.java
@@ -0,0 +1,40 @@
+package com.multithreading.practice.Example5using_java8;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		// Using Lambda expression to implement Runnable interface
+		Thread thread1 = new Thread(() -> {
+			for(int i=1; i<=5; i++) {
+				System.out.println(Thread.currentThread().getName() + " -> " + i);
+				
+				try {
+					Thread.sleep(1000);
+				} catch(InterruptedException e) {
+					System.out.println("IntruptedException occurs: " + e.getMessage());
+				}
+			}
+		});
+		
+		
+		// Using anonymous inner class
+		Thread thread2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(int i=1; i<=5; i++) {
+					System.out.println(Thread.currentThread().getName() + " -> " + i);
+					
+					try {
+						Thread.sleep(1000);
+					} catch(InterruptedException e) {
+						System.out.println("InterruptedException occurs: " + e.getMessage());
+					}
+				}
+			}
+		});
+		
+		
+		thread1.start();
+		thread2.start();
+	}
+}
