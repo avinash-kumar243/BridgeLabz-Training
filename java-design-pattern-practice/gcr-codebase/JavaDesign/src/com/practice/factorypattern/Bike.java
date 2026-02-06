14f4125a5ba58e19b39c7d13e29198056c27051c
Avinash kumar <avinashkumar40977@gmail.com>
2026-02-06 09:18:24 +0530

    [Avinash] : Added a practice program to implement and understand the Factory pattern

---
+++ java-design-pattern-practice/gcr-codebase/JavaDesign/src/com/practice/factorypattern/Bike.java
@@ -0,0 +1,9 @@
+package com.practice.factorypattern;
+
+public class Bike implements Vehicle {
+	
+	@Override
+	public void drive() {
+		System.out.println("Bike is driving"); 
+	}
+}
---
+++ java-design-pattern-practice/gcr-codebase/JavaDesign/src/com/practice/factorypattern/Vehicle.java
@@ -0,0 +1,5 @@
+package com.practice.factorypattern;
+
+interface Vehicle {
+	void drive();
+}
